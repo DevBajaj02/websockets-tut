8736b84f0a741730943dd038940c730d4b3ed958
Dev <devbajaj.bits@gmail.com>
2024-04-15 11:32:12 +0530

    Refactor server implementation using Express and WebSocketServer

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import WebSocket, { WebSocketServer } from 'ws';
-import http from 'http';
+import express from 'express'
+import { WebSocketServer, WebSocket } from 'ws'
 
-const server = http.createServer(function(request: any, response: any) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.end("hi there");
-});
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const wss = new WebSocketServer({ server });
+let httpServer =  app.listen(8080)
+
+const wss = new WebSocketServer({ server: httpServer });
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
@@ -20,8 +22,4 @@ wss.on('connection', function connection(ws) {
   });
 
   ws.send('Hello! Message From Server!!');
-});
-
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
 });
